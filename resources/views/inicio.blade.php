13304d94d5d2da2f8961809c6b73ce1f12574e20
Lucas Fonseca <16413626@adm.uepg.br>
2019-06-06 08:59:32 -0300

    Commit inicial

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,48 @@
+    @extends('layouts.menu')
+    @section('title', 'Nutriweb')
+    @section('content')
+    <main class="page-content">
+        <div class="container">
+            <h2>Bem vinda Fernanda</h2>
+            <img class="logo" src="img/logo.png">
+            <div class="row">
+                <div class="form-group col-md-12">
+                  <h5>Selecione paciente:</h5>
+                </div>
+                <div class="form-group col-md-8">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Digite aqui..." aria-label="Recipient's username" aria-describedby="button-addon2">
+                        <div class="input-group-append">
+                            <a href="" class="btn btn-outline-secondary" type="button" id="button-addon2">Busca
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h5>Cardápios a fazer:</h5>
+            <hr>
+            <div class="row">
+                <div class="col-md-8">
+                    <ul class="list-group">
+                        <h6>Paciente</h6>
+                        <li class="list-group-item">Paciente 1</li>
+                        <li class="list-group-item">Paciente 2</li>
+                        <li class="list-group-item">Paciente 3</li>
+                        <li class="list-group-item">Paciente 4</li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <ul class="list-group">
+                        <h6>Data</h6>
+                        <li class="list-group-item"> 1</li>
+                        <li class="list-group-item"> 2</li>
+                        <li class="list-group-item"> 3</li>
+                        <li class="list-group-item"> 4</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </main>
+    <!-- page-content" -->
+    @endsection
+
---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+          crossorigin="anonymous">
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/script.js"></script>
+    <script src="js/cadastro.js"></script>
+</head>
+<body>
+<!-- Início Menu -->
+<div class="page-wrapper chiller-theme toggled">
+    <a id="show-sidebar" class="btn btn-sm btn-dark" href="#">
+        <i class="fas fa-bars"></i>
+    </a>
+    <nav id="sidebar" class="sidebar-wrapper">
+        <div class="sidebar-content">
+            <div class="sidebar-brand">
+                <a href="inicio">NUTRIWEB</a>
+                <div id="close-sidebar">
+                    <i class="fas fa-times"></i>
+                </div>
+            </div>
+            <div class="sidebar-header">
+                <div class="user-pic">
+                    <img class="img-responsive img-rounded" src="https://raw.githubusercontent.com/azouaoui-med/pro-sidebar-template/gh-pages/src/img/user.jpg"
+                         alt="User picture" style="height: 80px; width: 60px">
+                </div>
+                <div class="user-info">
+          <span class="user-name">Fernanda
+            Fonseca
+          </span>
+                    <span class="user-role">Administrador</span>
+                    <span class="user-status">
+            <i class="fa fa-circle"></i>
+            <span>Online</span>
+          </span>
+                </div>
+            </div>
+            <!-- sidebar-header  -->
+            <div class="sidebar-search">
+                <div>
+                    <div class="input-group">
+                        <input type="text" class="form-control search-menu" placeholder="Buscar alimentos...">
+                        <div class="input-group-append">
+              <span class="input-group-text">
+                <i class="fa fa-search" aria-hidden="true"></i>
+              </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- sidebar-search  -->
+            <div class="sidebar-menu">
+                <ul>
+                    <li class="header-menu">
+                        <span>General</span>
+                    </li>
+                    <li class="sidebar-dropdown">
+                        <a href="#">
+                            <i class="fa fa-tachometer-alt"></i>
+                            <span>Atendimento</span>
+                        </a>
+                        <div class="sidebar-submenu">
+                            <ul>
+                                <li>
+                                    <a href="cadastro">Novo paciente
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">Reconsulta</a>
+                                </li>
+                                <li>
+                                    <a href="#">Relatório Paciente</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="sidebar-dropdown">
+                        <a href="#">
+                            <i class="fa fa-shopping-cart"></i>
+                            <span>Paciente</span>
+                        </a>
+                        <div class="sidebar-submenu">
+                            <ul>
+                                <li>
+                                    <a href="#">Products
+
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">Orders</a>
+                                </li>
+                                <li>
+                                    <a href="#">Credit cart</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="sidebar-dropdown">
+                        <a href="#">
+                            <i class="fa fa-globe"></i>
+                            <span>Dietas</span>
+                        </a>
+                        <div class="sidebar-submenu">
+                            <ul>
+                                <li>
+                                    <a href="#">Criar cardápios</a>
+                                </li>
+                                <li>
+                                    <a href="#">Criar grupos</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="sidebar-dropdown">
+                        <a href="/relatorio">
+                            <i class="fa fa-globe"></i>
+                            <span>Relatório</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <!-- sidebar-menu  -->
+        </div>
+        <!-- sidebar-content  -->
+        <div class="sidebar-footer">
+            <a href="#">
+                <i class="fa fa-bell"></i>
+            </a>
+            <a href="#">
+                <i class="fa fa-envelope"></i>
+            </a>
+            <a href="#">
+                <i class="fa fa-cog"></i>
+            </a>
+            <a href="#">
+                <i class="fa fa-power-off"></i>
+            </a>
+        </div>
+    </nav>
+    <!-- sidebar-wrapper  -->
+    <!-- Fim Menu -->
+    @yield('content')
+</div>
+<!-- page-wrapper -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
