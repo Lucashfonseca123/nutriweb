967327615ff528b07711b46efcd763350da4ee2c
Lucas Fonseca <16413626@adm.uepg.br>
2019-06-10 11:28:59 -0300

    Adicionado modelo de reconsulta

--- resources/views/paciente_reconsulta.blade.php
+++ resources/views/paciente_reconsulta.blade.php
@@ -4,8 +4,16 @@
         <div class="container">
             <form class="form-horizontal">
                 <fieldset>
-                <h3>TESTE PAGE CONTENT</h3>
-
+                <h3>TESTE PAGE CONTENT <br><br></h3>
+                    <div class="list-group">
+                        <a href="#" class="list-group-item list-group-item-action active">
+                            Cras justo odio
+                        </a>
+                        <a href="#" class="list-group-item list-group-item-action">Paciente 1</a>
+                        <a href="#" class="list-group-item list-group-item-action">Paciente 2</a>
+                        <a href="#" class="list-group-item list-group-item-action">Pàciente 3</a>
+                        <a href="#" class="list-group-item list-group-item-action disabled">Paciente 4</a>
+                    </div>
                 </fieldset>
             </form>
         </div>
