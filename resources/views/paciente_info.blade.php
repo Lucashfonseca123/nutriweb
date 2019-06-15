e5c16c57463cc80f8c4f237e7041e2e0dc48b67e
Lucas <lucashfonseca10@gmail.com>
2019-06-15 03:49:25 -0300

    Adicionado infos do paciente

--- resources/views/paciente_info.blade.php
+++ resources/views/paciente_info.blade.php
@@ -3,8 +3,9 @@
     <main class="page-content">
         <div class="container">
             <form class="form-horizontal">
-                <h4 class="col">Editar grupos</h4><br>
-                <h6 class="col">Selecione grupo</h6>
+                <h4 class="col">Editar paciente</h4><br>
+                <img class="logo3" src="img/logo.png">
+                <h6 class="col">Selecione paciente</h6>
                 <div class="form-group col-md-6">
                     <div class="input-group">
                         <input type="text" class="form-control" id="pessoas" placeholder="Digite aqui..." aria-label="Recipient's username" aria-describedby="button-addon2">
@@ -13,16 +14,12 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-5 card-header text-center escondido" id="revelado">
-                    <div class="form-group text-center">
-                        <label for=""><h6>Infos paciente 1</h6></label>
-                    </div>
-                    <div class="form-group text-center">
-                        <label for="">
-                            <input type="text" id="Nome" style="width: 180px" class="form-control" placeholder="Gráfico aqui" disabled>
-                        </label>
-                    </div>
-                  </div>
+                <div>
+                <div class="form-group escondido " id="revelado">
+                    <label for="" class="col">
+                        <input type="text" style="width: 180px" id="alimentos" class="form-control" placeholder="Tem um gráfico aqui.." disabled>
+                    </label>
+                </div>
             </form>
         </div>
     </main>
