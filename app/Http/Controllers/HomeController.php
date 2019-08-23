4b00838484c00eb744762ccd7ac2e979a282d8d9
Lucas <lucashfonseca10@gmail.com>
2019-08-23 11:00:36 -0300

    Retirado migration erradas

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,8 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $teste = Teste2::all();
-        return view('home');
+
+        return view('welcome');
     }
 
 }
